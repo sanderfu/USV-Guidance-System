eddb15818445b97cfe4c8861dad0e3ddbcd1bcdf
Sander Furre <sanderfu@stud.ntnu.no>
2022-03-08 09:38:33 +0100

    Update debug files, add testmission and mission region

--- usv_mission_planner/scripts/debug_astar.py
+++ usv_mission_planner/scripts/debug_astar.py
@@ -50,7 +50,9 @@ GREEN = '#4F7942'
 
 def main():
     rospack = rospkg.RosPack()
-    datasource_path = rospack.get_path('usv_simulator')+"/maps/check_db.sqlite"
+    map_name = "outside_new_york"
+    mission_name = "testmission"
+    datasource_path = rospack.get_path('usv_map')+"/data/mission_regions/"+map_name+"/check_db.sqlite"
     ds:gdal.Dataset = gdal.OpenEx(datasource_path)
     if ds==None:
         raise RuntimeError("Failed to load datasource",datasource_path)
@@ -67,15 +69,9 @@ def main():
             patch1 = PolygonPatch(poly, fc=GREEN, ec=GREEN, alpha=1, zorder=2)
             ax.add_patch(patch1)
 
-    #Plot distance coloured
-    distance_path = rospack.get_path('usv_map')+"/data/debug_distance_concept/distance_tiles.csv"
-    distance_df = pd.read_csv(distance_path)
-    z = distance_df["distance"]
-    z_normalized = (z-min(z))/(max(z)-min(z))
-
-    tile_size = 0.0005/2
-    colors = plt.cm.get_cmap("viridis",len(np.unique(z_normalized.round(decimals=4)))*2)
-    i=0
+    #tile_size = 0.0005/2
+    #colors = plt.cm.get_cmap("viridis",len(np.unique(z_normalized.round(decimals=4)))*2)
+    #i=0
     #for index,row in tqdm(distance_df.iterrows(), total=distance_df.shape[0]):
     #    x,y = row["x_center"], row["y_center"]
     #    coords = np.array([[x-tile_size,y-tile_size],[x-tile_size,y+tile_size],[x+tile_size,y+tile_size],[x+tile_size,y-tile_size]])
@@ -86,34 +82,36 @@ def main():
     #    if(i>10000):
     #        break
 
+    #print("Plot path")
     #Plot path
     path_path = rospack.get_path('usv_mission_planner')+"/data/debug/astar/path.csv"
     path_df = pd.read_csv(path_path)
-    ax.plot(path_df["lon"],path_df["lat"],color="red",zorder=3)
+    ax.plot(path_df["lon"],path_df["lat"],color="red",zorder=3,label="path")
 
+    #print("Came from line segments")
     #Plot came-from line segments
-    came_from_path = rospack.get_path('usv_mission_planner')+"/data/debug/astar/came_from.csv"
-    came_from_df = pd.read_csv(came_from_path)
-    lines = []
-    for index,row in came_from_df.iterrows():
-        line = [(row["lon_from"],row["lat_from"]),(row["lon_to"],row["lat_to"])]
-        lines.append(line)
-    lc = mc.LineCollection(lines, linewidths=0.1,zorder=3)
-    ax.add_collection(lc)
+    #came_from_path = rospack.get_path('usv_mission_planner')+"/data/debug/astar/came_from.csv"
+    #came_from_df = pd.read_csv(came_from_path)
+    #lines = []
+    #for index,row in came_from_df.iterrows():
+    #    line = [(row["lon_from"],row["lat_from"]),(row["lon_to"],row["lat_to"])]
+    #    lines.append(line)
+    #lc = mc.LineCollection(lines, linewidths=0.1,zorder=3)
+    #ax.add_collection(lc)
 
     #Plot closed
     closed_path = rospack.get_path('usv_mission_planner')+"/data/debug/astar/closed.csv"
     closed_df = pd.read_csv(closed_path)
-    ax.scatter(closed_df["lon"],closed_df["lat"],color="grey",zorder=3)
+    ax.scatter(closed_df["lon"],closed_df["lat"],color="grey",zorder=3,label="closed")
 
     #Plot unexplored
     frontier_path = rospack.get_path('usv_mission_planner')+"/data/debug/astar/frontier.csv"
     frontier_df = pd.read_csv(frontier_path)
-    ax.scatter(frontier_df["lon"],frontier_df["lat"],color="blue",zorder=3)
+    ax.scatter(frontier_df["lon"],frontier_df["lat"],color="blue",zorder=3,label="frontier")
 
 
     #Plot quadtree
-    quadtree_path = rospack.get_path('usv_mission_planner')+"/data/quadtrees/test_quadtree/test_quadtree.csv"
+    quadtree_path = rospack.get_path('usv_map')+"/data/mission_regions/"+map_name+"/quadtree.csv"
     quadtree_df = pd.read_csv(quadtree_path)
     lines = []
     for index,row in quadtree_df.iterrows():
@@ -121,6 +119,7 @@ def main():
         lines.append(line)
     lc = mc.LineCollection(lines, linewidths=0.1,zorder=3)
     ax.add_collection(lc)
+    ax.legend()
 
 
     plt.autoscale(enable=True, axis="both", tight=None)
--- usv_mission_planner/scripts/debug_hybrid_astar.py
+++ usv_mission_planner/scripts/debug_hybrid_astar.py
@@ -50,7 +50,9 @@ GREEN = '#4F7942'
 
 def main():
     rospack = rospkg.RosPack()
-    datasource_path = rospack.get_path('usv_simulator')+"/maps/check_db.sqlite"
+    map_name = "outside_new_york"
+    mission_name = "testmission"
+    datasource_path = rospack.get_path('usv_map')+"/data/mission_regions/"+map_name+"/check_db.sqlite"
     ds:gdal.Dataset = gdal.OpenEx(datasource_path)
     if ds==None:
         raise RuntimeError("Failed to load datasource",datasource_path)
@@ -76,21 +78,21 @@ def main():
     tile_size = 0.001/2
     colors = plt.cm.get_cmap("Greys",len(np.unique(z_normalized.round(decimals=4)))*2)
     i=0
-    for index,row in tqdm(distance_df.iterrows(), total=distance_df.shape[0]):
-        x,y = row["x_center"], row["y_center"]
-        coords = np.array([[x-tile_size,y-tile_size],[x-tile_size,y+tile_size],[x+tile_size,y+tile_size],[x+tile_size,y-tile_size]])
-        poly:Polygon = Polygon(coords)
-        patch1 = PolygonPatch(poly, fc=mpl.colors.rgb2hex(colors(z_normalized[index])),ec=mpl.colors.rgb2hex(colors(z_normalized[index])), alpha=1, zorder=1)
-        ax.add_patch(patch1)
-        i+=1
+    #for index,row in tqdm(distance_df.iterrows(), total=distance_df.shape[0]):
+    #    x,y = row["x_center"], row["y_center"]
+    #    coords = np.array([[x-tile_size,y-tile_size],[x-tile_size,y+tile_size],[x+tile_size,y+tile_size],[x+tile_size,y-tile_size]])
+    #    poly:Polygon = Polygon(coords)
+    #    patch1 = PolygonPatch(poly, fc=mpl.colors.rgb2hex(colors(z_normalized[index])),ec=mpl.colors.rgb2hex(colors(z_normalized[index])), alpha=1, zorder=1)
+    #    ax.add_patch(patch1)
+    #    i+=1
 
     #Plot path
-    path_path = rospack.get_path('usv_mission_planner')+"/data/debug/hybrid_astar/path.csv"
+    path_path = rospack.get_path('usv_mission_planner')+"/data/missions/"+mission_name+"/hybrid_astar/path.csv"
     path_df = pd.read_csv(path_path)
-    ax.plot(path_df["lon"],path_df["lat"],color="red",zorder=3)
+    ax.plot(path_df["lon"],path_df["lat"],color="red",zorder=3,label="path")
 
     #Plot came-from line segments
-    came_from_path = rospack.get_path('usv_mission_planner')+"/data/debug/hybrid_astar/came_from.csv"
+    came_from_path = rospack.get_path('usv_mission_planner')+"/data/missions/"+mission_name+"/hybrid_astar/came_from.csv"
     came_from_df = pd.read_csv(came_from_path)
     lines = []
     for index,row in came_from_df.iterrows():
@@ -100,8 +102,8 @@ def main():
     ax.add_collection(lc)
 
     #Plot closed
-    #plotpause = 0.00001
-    closed_path = rospack.get_path('usv_mission_planner')+"/data/debug/hybrid_astar/closed.csv"
+    plotpause = 0.00001
+    closed_path = rospack.get_path('usv_mission_planner')+"/data/missions/"+mission_name+"/hybrid_astar/closed.csv"
     closed_df = pd.read_csv(closed_path)
     #for index, row in closed_df.iterrows():
     #    point = ax.scatter(row["lon"],row["lat"],color="grey",zorder=3)
@@ -110,20 +112,20 @@ def main():
     #    plt.waitforbuttonpress(plotpause)
     #    point.remove()
 
-    ax.scatter(closed_df["lon"],closed_df["lat"],color="grey",zorder=3)
+    ax.scatter(closed_df["lon"],closed_df["lat"],color="grey",zorder=3,label="closed")
 
     #Plot unexplored
-    frontier_path = rospack.get_path('usv_mission_planner')+"/data/debug/hybrid_astar/frontier.csv"
+    frontier_path = rospack.get_path('usv_mission_planner')+"/data/missions/"+mission_name+"/hybrid_astar/frontier.csv"
     frontier_df = pd.read_csv(frontier_path)
-    ax.scatter(frontier_df["lon"],frontier_df["lat"],color="blue",zorder=3)
+    ax.scatter(frontier_df["lon"],frontier_df["lat"],color="blue",zorder=3,label="frontier")
 
     #Plot vertices outside quadtree
-    outside_path = rospack.get_path('usv_mission_planner')+"/data/debug/hybrid_astar/outside_quadtree.csv"
+    outside_path = rospack.get_path('usv_mission_planner')+"/data/missions/"+mission_name+"/hybrid_astar/outside_quadtree.csv"
     outside_df = pd.read_csv(outside_path)
-    ax.scatter(outside_df["lon"],outside_df["lat"],color="red",marker="x",zorder=3)
+    ax.scatter(outside_df["lon"],outside_df["lat"],color="red",marker="x",zorder=3,label="fastcheck")
 
     #Plot quadtree
-    quadtree_path = rospack.get_path('usv_mission_planner')+"/data/quadtrees/test_quadtree/test_quadtree.csv"
+    quadtree_path = rospack.get_path('usv_map')+"/data/mission_regions/"+map_name+"/quadtree.csv"
     quadtree_df = pd.read_csv(quadtree_path)
     lines = []
     for index,row in quadtree_df.iterrows():
@@ -131,6 +133,7 @@ def main():
         lines.append(line)
     lc = mc.LineCollection(lines, linewidths=0.1,zorder=3)
     ax.add_collection(lc)
+    ax.legend()
 
 
     plt.autoscale(enable=True, axis="both", tight=None)

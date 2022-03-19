2e9bec23c722bdcb6d4455bd68203cb1059c5d2c
Sander Furre <sanderfu@stud.ntnu.no>
2022-03-19 18:56:06 +0100

    Use more accurate collision checking
    This makes it possible to build quadtree and voronoi skeleton at the same time (remains to be implemented)

--- usv_map/src/skeleton_generator.cpp
+++ usv_map/src/skeleton_generator.cpp
@@ -49,97 +49,6 @@ void VoronoiSkeletonGenerator::run(){
     //std::unordered_map<std::pair<double,double>,bool,hash_pair> contained_in_geometry_lookup;
     pruneEdges(diagram);
     jcv_diagram_free( &diagram );
-    return;
-    while(edges){
-        bool intersects = false;
-        total_edges++;
-        if(total_edges%1000==0){
-            std::cout << "Total edges processed: " << total_edges << std::endl;
-        }
-        
-        if(tree_->getLeafRegionContaining(edges->pos[0].x,edges->pos[0].y)==nullptr || tree_->getLeafRegionContaining(edges->pos[1].x,edges->pos[1].y)==nullptr){
-            fast_check_edges++;
-            edges = jcv_diagram_get_next_edge(edges);
-            continue;
-        }        
-        
-        //Try to prune branches that are approximately normal to land
-        double edge_length;
-        geod_->Inverse(edges->pos[0].y,edges->pos[0].x,edges->pos[1].y,edges->pos[1].x,edge_length);
-        edge_length=abs(edge_length);
-
-        double d0=map_service_->distance(edges->pos[0].x,edges->pos[0].y,LayerID::COLLISION,INFINITY);
-        double d1=map_service_->distance(edges->pos[1].x,edges->pos[1].y,LayerID::COLLISION,INFINITY);
-        double diff = abs(abs(d1)-abs(d0))*1e5;
-        //std::cout << abs(edge_length/diff) << std::endl;
-        
-        
-        if(abs(edge_length/diff)<=1.1){
-            branches_pruned++;
-            edges = jcv_diagram_get_next_edge(edges);
-            continue;
-        }
-        
-        
-        
-
-        //std::cout << "Edge length: " << edge_length << " Diff: " << diff*1e5 << " Difference: " << abs(edge_length-diff) << std::endl;
-        
-        /*
-        auto contained_in_geometry_a = contained_in_geometry_lookup.find(std::make_pair(edges->pos[0].x,edges->pos[0].y));
-        auto contained_in_geometry_b = contained_in_geometry_lookup.find(std::make_pair(edges->pos[1].x,edges->pos[1].y));
-
-        if(contained_in_geometry_a!=contained_in_geometry_lookup.end() || contained_in_geometry_b!=contained_in_geometry_lookup.end()){
-            //One of the points are registered to be in land geometry, can skip check and set intersects=true
-            intersects=true;
-        } 
-        */
-       
-       
-        check_point_a.setX(edges->pos[0].x);
-        check_point_a.setY(edges->pos[0].y);
-        check_point_b.setX(edges->pos[1].x);
-        check_point_b.setY(edges->pos[1].y);
-
-        in_layer_->ResetReading();
-        while((feat = in_layer_->GetNextFeature()) != NULL){
-            geom = feat->GetGeometryRef();
-            bool check_point_a_intersects = geom->Contains(&check_point_a);
-            bool check_point_b_intersects = geom->Contains(&check_point_b);
-            /*
-            if(check_point_a_intersects){
-                contained_in_geometry_lookup.insert(std::make_pair(std::make_pair(edges->pos[0].x,edges->pos[0].y),true));
-                intersects=true;
-            }
-            if(check_point_b_intersects){
-                contained_in_geometry_lookup.insert(std::make_pair(std::make_pair(edges->pos[1].x,edges->pos[1].y),true));
-                intersects=true;
-            }
-            */
-            if(intersects) break;
-            OGRFeature::DestroyFeature(feat);
-        }
-        
-        if (intersects){
-            edges = jcv_diagram_get_next_edge(edges);
-            continue;
-        }
-        
-        
-        //If comes this far, add
-        /*OGRLineString line;
-        line.addPoint(&check_point_a);
-        line.addPoint(&check_point_b);
-        voronoi_geom.addGeometry(&line);
-        */
-
-        //i++;
-        //edge_file << edges->pos[0].x << "," << edges->pos[0].y << "," << edges->pos[1].x << "," << edges->pos[1].y << "\n";
-        edges = jcv_diagram_get_next_edge(edges);
-    }
-    //voronoi_feature->SetGeometry(&voronoi_geom);
-    //voronoi_layer_->CreateFeature(voronoi_feature);
-    jcv_diagram_free( &diagram );
 }
 
 void VoronoiSkeletonGenerator::pruneEdges(jcv_diagram diagram){
@@ -152,10 +61,26 @@ void VoronoiSkeletonGenerator::pruneEdges(jcv_diagram diagram){
     int edge_counter = 0;
 
     int precision = 1e6;
+    OGRPoint check_point_a;
+    OGRPoint check_point_b;
     while(edges){
 
         //First check for completely invalid edges that atually collide with land and remove them
+        /*
         if(tree_->getLeafRegionContaining(edges->pos[0].x,edges->pos[0].y)==nullptr || tree_->getLeafRegionContaining(edges->pos[1].x,edges->pos[1].y)==nullptr){
+            edges = jcv_diagram_get_next_edge(edges);
+            continue;
+        }
+        */
+
+        check_point_a.setX(edges->pos[0].x);
+        check_point_a.setY(edges->pos[0].y);
+        check_point_b.setX(edges->pos[1].x);
+        check_point_b.setY(edges->pos[1].y);
+        OGRLineString line;
+        line.addPoint(&check_point_a);
+        line.addPoint(&check_point_b);
+        if(map_service_->intersects(&line,LayerID::COLLISION)){
             edges = jcv_diagram_get_next_edge(edges);
             continue;
         }
@@ -168,8 +93,6 @@ void VoronoiSkeletonGenerator::pruneEdges(jcv_diagram diagram){
         tmp_point_pair.first = edges->pos[0].x*precision;
         tmp_point_pair.second = edges->pos[0].y*precision;
         point_map[tmp_point_pair].push_back(id);
-        std::cout << edges->pos[0].x << " " << edges->pos[0].y << std::endl;
-        std::cout << tmp_point_pair.first << " " << tmp_point_pair.second << std::endl;
         tmp_point_pair.first = edges->pos[1].x*precision;
         tmp_point_pair.second = edges->pos[1].y*precision;
         point_map[tmp_point_pair].push_back(id);
@@ -178,6 +101,7 @@ void VoronoiSkeletonGenerator::pruneEdges(jcv_diagram diagram){
     }
 
     //Check if any points are super close (less than 1 cm)
+    /*
     std::cout << "Checking if any point is super close to other point" << std::endl;
     double dist = 0;
     double smallest_dist = INFINITY;
@@ -193,6 +117,7 @@ void VoronoiSkeletonGenerator::pruneEdges(jcv_diagram diagram){
         }
     }
     std::cout << "Smallest distance measured:" << smallest_dist << std::endl;
+    */
 
     //Identify prune candidates
     while(true){
@@ -257,10 +182,6 @@ void VoronoiSkeletonGenerator::pruneEdges(jcv_diagram diagram){
     OGRFeature* voronoi_feature = OGRFeature::CreateFeature(voronoi_layer_->GetLayerDefn());
     voronoi_feature->SetFID(0);
     OGRMultiLineString voronoi_geom;
-    OGRPoint check_point_a;
-    OGRPoint check_point_b;
-
-
 
     for(auto it = unique_remaining_edges.begin(); it!= unique_remaining_edges.end(); it++){
         //Add unique edges
